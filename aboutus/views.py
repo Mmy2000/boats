00b2b0ffc6553c6c48b6a765222600677598348b
Mmy2000 <yahianada005@gmail.com>
2024-07-25 13:34:40 +0300

    adding functionallity and models for about app

--- aboutus/views.py
+++ aboutus/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .models import AboutUS
+from .models import AboutUS , FAQ
 # Create your views here.
 
 def about(request):
     about = AboutUS.objects.last()
+    faqs = FAQ.objects.all()
     context = {
-        'about':about
+        'about':about,
+        'faqs':faqs
     }
     return render(request , 'about_us.html' , context)
