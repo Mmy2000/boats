e32208b7047cfd05e33aeb7eacc4c8a4489cde90
Mmy2000 <yahianada005@gmail.com>
2024-07-28 13:39:33 +0300

    creating home page

--- settings/views.py
+++ settings/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from products.models import Product
+from aboutus.models import AboutUS , FAQ
+from services.models import Services
 # Create your views here.
 def home(request):
-    return render(request , 'home.html')
+    about = AboutUS.objects.last()
+    faqs = FAQ.objects.all()
+    services = Services.objects.all()[:4]
+
+    context = {
+        'about':about,
+        'faqs':faqs,
+        'services':services,
+    }
+    return render(request , 'home.html' , context)
 
